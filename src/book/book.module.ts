c7b34bce4cd0526d17d3f0a16451113340298aa9
Han Solo <aunghansoe.mdy@gmail.com>
2023-05-08 15:28:03 +0630

    fet: add rest api controllers for book

--- src/book/book.module.ts
+++ src/book/book.module.ts
@@ -3,9 +3,11 @@ import { AuthorModule } from 'src/author/author.module';
 import { PrismaService } from 'src/prisma.service';
 import { BookResolver } from './book.resolver';
 import { BookService } from './book.service';
+import { BookController } from './book.controller';
 
 @Module({
     imports: [AuthorModule],
-    providers: [BookService, BookResolver, PrismaService]
+    providers: [BookService, BookResolver, PrismaService],
+    controllers: [BookController]
 })
 export class BookModule {}
